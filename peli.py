34ddeb7366ce7ef53b2c05f7bde6f5a7eb95c710
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2021-12-13 14:31:17 +0200

    Tuomas versio

--- peli.py
+++ peli.py
@@ -1,15 +1,16 @@
 import math
+from random import randint, random
+
 import pygame
-from pygame.constants import K_LEFT, K_RIGHT
 
 TAUSTAVARI = (180, 180, 240)  # (Red, Green, Blue), 0...255
 
- 
+
 def main():
     peli = Peli()
     peli.aja()
- 
- 
+
+
 class Peli:
     def __init__(self):
         self.ajossa = True
@@ -17,8 +18,6 @@ class Peli:
         self.leveys = 1280
         self.korkeus = 720
         self.nayton_koko = (self.leveys, self.korkeus)
-        # self.nayton_koko = (self.weight, self.height) = (800, 600)
-
  
     def aja(self):
         self.alustus()
@@ -28,114 +27,120 @@ class Peli:
             self.pelilogiikka()
             self.renderointi()
         self.lopetus()
- 
+
     def alustus(self):
         pygame.init()
         self.kello = pygame.time.Clock()
+        self.kokoruutu = False
         self.naytto = pygame.display.set_mode(
             self.nayton_koko, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        # self.kuva_iso = pygame.image.load("rocket_640r2.png")
-        self.kuva_iso = pygame.image.load("man.gif")
-        # self.kuva = pygame.transform.scale(self.kuva, (512, 512)) # image size
-        self.kuva_pieni = pygame.transform.rotozoom(self.kuva_iso, 0, 0.5) # image rotation and size
-        self.kulma = 0
-        self.pyorimisvauhti = 0
-        self.voima = 0
-        self.sijainti = (400, 300)
+        self.raketti_iso = pygame.image.load("rocket_640r2.png")
+        self.raketti_pieni = pygame.transform.rotozoom(self.raketti_iso, 0, 0.25)
+        self.juttu_iso = pygame.image.load("man.gif")
+        self.juttu_pieni = pygame.transform.rotozoom(self.juttu_iso, 0, 0.4)
+        self.raketin_kulma = 0
+        self.raketin_pyorimisvauhti = 0
+        self.raketin_sijainti = (400, 300)
+        self.arvo_uusi_juttu()
         self.vauhti = 0
-        self.kokoruutu = False
         self.hiiren_nappi_pohjassa = False
+        self.voima = 0
         self.voimanlisays = False
         self.laukaisu = False
- 
+
+    def arvo_uusi_juttu(self):        
+        self.jutun_kulma = 0
+        self.jutun_pyorimisvauhti = random() - 0.5
+        self.jutun_sijainti = (randint(0, self.leveys), randint(0, self.korkeus))
+
     def tapahtuma(self, event):
         if event.type == pygame.QUIT:
             self.ajossa = False
+        # Hiiren tapahtumat --------------------------------------
         elif event.type == pygame.MOUSEBUTTONDOWN:
             self.hiiren_nappi_pohjassa = True
         elif event.type == pygame.MOUSEBUTTONUP:
             self.hiiren_nappi_pohjassa = False
-            # self.sijainti = pygame.mouse.get_pos() # расположение картинки по положению мышки
-
+        # Näppäimen painaminen alas ------------------------------
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                self.pyorimisvauhti = 3
+                self.raketin_pyorimisvauhti = 3
             elif event.key == pygame.K_RIGHT:
-                self.pyorimisvauhti = -3
+                self.raketin_pyorimisvauhti = -3
             elif event.key == pygame.K_SPACE:
                 self.voimanlisays = True
-
+        # Näppäimen nosto ylös  ----------------------------------
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_ESCAPE:
                 self.ajossa = False
             elif event.key == pygame.K_F11:
                 self.vaihda_kokoruututila()
-
             elif event.key in (pygame.K_LEFT, pygame.K_RIGHT):
-                self.pyorimisvauhti = 0
+                self.raketin_pyorimisvauhti = 0
             elif event.key == pygame.K_SPACE:
                 self.voimanlisays = False
-                self.laukaisu = True
-            
+                self.laukaisu = True           
 
- 
     def pelilogiikka(self):
         if self.hiiren_nappi_pohjassa:
-            self.sijainti = pygame.mouse.get_pos() # расположение картинки по положению мышки
-        if self.pyorimisvauhti != 0:
-            self.kulma = (self.kulma + self.pyorimisvauhti) % 360
+            self.raketin_sijainti = pygame.mouse.get_pos()
+
+        if self.raketin_pyorimisvauhti != 0:
+            self.raketin_kulma = (self.raketin_kulma + self.raketin_pyorimisvauhti) % 360
+
+        if self.jutun_pyorimisvauhti != 0:
+            self.jutun_kulma = (self.jutun_kulma + self.jutun_pyorimisvauhti) % 360
 
         if self.voimanlisays:
             self.voima = min(self.voima + 2, 100)
-            # print (self.voima)
-        
+
         if self.laukaisu:
-            # print (f"Pam {self.voima}")
-            self.vauhti = self.voima ** 2 / 100
+            self.vauhti = self.voima ** 2 / 200.0
             self.voima = 0
             self.laukaisu = False
-        
+
         if self.vauhti > 0.1:
-            vauhti_x = -self.vauhti * math.sin(self.kulma / 180 * math.pi)
-            vauhti_y = -self.vauhti * math.cos(self.kulma / 180 * math.pi)
-            uusi_x = self.sijainti[0] + vauhti_x
-            uusi_y = self.sijainti[1] + vauhti_y
-            self.sijainti = (uusi_x, uusi_y)
-            self.vauhti *= 0.99 # затухание
+            vauhti_x = -self.vauhti * math.sin(self.raketin_kulma / 180 * math.pi)
+            vauhti_y = -self.vauhti * math.cos(self.raketin_kulma / 180 * math.pi)
+            uusi_x = self.raketin_sijainti[0] + vauhti_x
+            uusi_y = self.raketin_sijainti[1] + vauhti_y
+            self.raketin_sijainti = (uusi_x, uusi_y)
+            self.vauhti *= 0.97
 
-        # self.kulma -= ((360/60)/6)
-        # self.kulma = (self.kulma - 3) % 360 # вращение картинки
-        # self.kulma = 0
- 
+        etaisyys_2 = (
+            (self.raketin_sijainti[0] - self.jutun_sijainti[0])**2 +
+            (self.raketin_sijainti[1] - self.jutun_sijainti[1])**2)
+        if etaisyys_2 < 5000:
+            self.arvo_uusi_juttu()
+
     def renderointi(self):
-        self.naytto.fill(TAUSTAVARI) # заливка экрана RGB
-        kuva = pygame.transform.rotozoom(self.kuva_pieni, self.kulma, 1)
-        # keskipiste = self.kuva_pieni.get_rect(topleft=(0, 0)).center # make center picture
-        # laatikko = kuva.get_rect(center=keskipiste)
-        laatikko = kuva.get_rect(center=self.sijainti) 
-        self.naytto.blit(kuva, laatikko.topleft) # image coordinates
-
-        pygame.draw.rect(self.naytto, (0, 0, 0), (2, self.korkeus-19, 102, 17))
-        pygame.draw.rect(self.naytto, (0, 255, 0), (3, self.korkeus-18, self.voima, 15))
+        # Tausta
+        self.naytto.fill(TAUSTAVARI)
+        # Juttu
+        kuva = pygame.transform.rotozoom(self.juttu_pieni, self.jutun_kulma, 1)
+        laatikko = kuva.get_rect(center=self.jutun_sijainti)
+        self.naytto.blit(kuva, laatikko.topleft)
+        # Raketti
+        kuva = pygame.transform.rotozoom(self.raketti_pieni, self.raketin_kulma, 1)
+        laatikko = kuva.get_rect(center=self.raketin_sijainti)
+        self.naytto.blit(kuva, laatikko.topleft)
+        # Voimapalkki
+        pygame.draw.rect(self.naytto, (0, 0, 0), (2, self.korkeus - 19, 102, 17))
+        pygame.draw.rect(self.naytto, (0, 255, 0), (3, self.korkeus - 18, self.voima, 15))
+        # Suuntapallo
         suuntapallo_x = self.leveys - 35
         suuntapallo_y = self.korkeus - 35
-        vektori_x = -30 * math.sin(self.kulma / 180 * math.pi)
-        vektori_y = -30 * math.cos(self.kulma / 180 * math.pi)
-        suuntapallo_x = self.leveys - 30
-        suuntapallo_y = self.korkeus - 30
+        suuntavektori_x = -30 * math.sin(self.raketin_kulma / 180 * math.pi)
+        suuntavektori_y = -30 * math.cos(self.raketin_kulma / 180 * math.pi)
         pygame.draw.circle(self.naytto, (0, 0, 0), (suuntapallo_x, suuntapallo_y), 30)
-        pygame.draw.line(self.naytto, (255, 0, 0), (suuntapallo_x, suuntapallo_y), (suuntapallo_x + vektori_x, suuntapallo_y + vektori_y))
+        pygame.draw.line(self.naytto, (255, 0, 0),
+                        (suuntapallo_x, suuntapallo_y),
+                        (suuntapallo_x + suuntavektori_x, suuntapallo_y + suuntavektori_y))
+        # Päivitä ruutu
         pygame.display.flip()
+        self.kello.tick(60)  # 60 fps (frames per second)
 
-        # print('laatikko :', laatikko)
-        # print('laatikko.topleft :', laatikko.topleft)
-        # print('laatikko.bottomright :', laatikko.bottomright)
-        # print('laatikko.center :', laatikko.center)
-        self.kello.tick(60)  # 60 fps
-        # if self.kulma % 100 == 0:
-        #     print(self.kello.get_fps()) # текущий fps
-
-    def vaihda_kokoruututila (self):
+    def vaihda_kokoruututila(self):
         self.kokoruutu = not self.kokoruutu
         if self.kokoruutu:
             pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -145,11 +150,9 @@ class Peli:
         self.leveys = naytto.get_width()
         self.korkeus = naytto.get_height()
 
-
- 
     def lopetus(self):
         pygame.quit()
  
- 
+
 if __name__ == "__main__" :
     main()
