ffa931517cd0ce0573da4a3735db3f6dc139a33c
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2021-12-13 11:56:25 +0200

    new verssion

--- peli.py
+++ peli.py
@@ -1,4 +1,6 @@
+import math
 import pygame
+from pygame.constants import K_LEFT, K_RIGHT
 
 TAUSTAVARI = (180, 180, 240)  # (Red, Green, Blue), 0...255
 
@@ -35,26 +37,68 @@ class Peli:
         # self.kuva_iso = pygame.image.load("rocket_640r2.png")
         self.kuva_iso = pygame.image.load("man.gif")
         # self.kuva = pygame.transform.scale(self.kuva, (512, 512)) # image size
-        self.kuva_pieni = pygame.transform.rotozoom(self.kuva_iso, -45, 0.25) # image rotation and size
+        self.kuva_pieni = pygame.transform.rotozoom(self.kuva_iso, 0, 0.5) # image rotation and size
         self.kulma = 0
+        self.pyorimisvauhti = 0
+        self.voima = 0
         self.sijainti = (400, 300)
-        self.nappi_pohjassa = False
+        self.vauhti = 0
+        self.hiiren_nappi_pohjassa = False
+        self.voimanlisays = False
+        self.laukaisu = False
  
     def tapahtuma(self, event):
         if event.type == pygame.QUIT:
             self.ajossa = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            self.nappi_pohjassa = True
+            self.hiiren_nappi_pohjassa = True
         elif event.type == pygame.MOUSEBUTTONUP:
-            self.nappi_pohjassa = False
+            self.hiiren_nappi_pohjassa = False
             # self.sijainti = pygame.mouse.get_pos() # расположение картинки по положению мышки
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.pyorimisvauhti = 3
+            elif event.key == pygame.K_RIGHT:
+                self.pyorimisvauhti = -3
+            elif event.key == pygame.K_SPACE:
+                self.voimanlisays = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+                self.pyorimisvauhti = 0
+            elif event.key == pygame.K_SPACE:
+                self.voimanlisays = False
+                self.laukaisu = True
+
  
     def pelilogiikka(self):
-        if self.nappi_pohjassa:
+        if self.hiiren_nappi_pohjassa:
             self.sijainti = pygame.mouse.get_pos() # расположение картинки по положению мышки
+        if self.pyorimisvauhti != 0:
+            self.kulma = (self.kulma + self.pyorimisvauhti) % 360
+
+        if self.voimanlisays:
+            self.voima = min(self.voima + 1, 100)
+            # print (self.voima)
+        
+        if self.laukaisu:
+            # print (f"Pam {self.voima}")
+            self.vauhti = self.voima / 10
+            self.voima = 0
+            self.laukaisu = False
+        
+        if self.vauhti > 0.1:
+            vauhti_x = -self.vauhti * math.sin(self.kulma / 180 * math.pi)
+            vauhti_y = -self.vauhti * math.cos(self.kulma / 180 * math.pi)
+            uusi_x = self.sijainti[0] + vauhti_x
+            uusi_y = self.sijainti[1] + vauhti_y
+            self.sijainti = (uusi_x, uusi_y)
+            self.vauhti *= 0.99
+
         # self.kulma -= ((360/60)/6)
-        self.kulma = (self.kulma - 3) % 360
-        self.kulma = 0
+        # self.kulma = (self.kulma - 3) % 360 # вращение картинки
+        # self.kulma = 0
  
     def renderointi(self):
         self.naytto.fill(TAUSTAVARI) # заливка экрана RGB
@@ -63,6 +107,16 @@ class Peli:
         # laatikko = kuva.get_rect(center=keskipiste)
         laatikko = kuva.get_rect(center=self.sijainti) 
         self.naytto.blit(kuva, laatikko.topleft) # image coordinates
+        pygame.draw.rect(self.naytto, (0, 0, 0), (2, self.korkeus-19, 102, 17))
+        pygame.draw.rect(self.naytto, (0, 255, 0), (3, self.korkeus-18, self.voima, 15))
+        suuntapallo_x = self.leveys - 35
+        suuntapallo_y = self.korkeus - 35
+        vektori_x = -30 * math.sin(self.kulma / 180 * math.pi)
+        vektori_y = -30 * math.cos(self.kulma / 180 * math.pi)
+        suuntapallo_x = self.leveys - 30
+        suuntapallo_y = self.korkeus - 30
+        pygame.draw.circle(self.naytto, (0, 0, 0), (suuntapallo_x, suuntapallo_y), 30)
+        pygame.draw.line(self.naytto, (255, 0, 0), (suuntapallo_x, suuntapallo_y), (suuntapallo_x + vektori_x, suuntapallo_y + vektori_y))
         pygame.display.flip()
 
         # print('laatikko :', laatikko)
